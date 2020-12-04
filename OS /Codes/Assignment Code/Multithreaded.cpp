cb5a5b614df347e9c1ea225098848e1fe49567c4
Srikanth <Srikanthkandarp23@gmail.com>
2020-12-04 21:54:51 +0530

    Codes renamed OS

---
+++ OS /Codes/Assignment Code/Multithreaded.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <thread>
+#include <time.h>
+#define rows 100
+#define columns 100
+#define threads_count 100
+int matrixt1[rows][columns];
+int matrixt2[rows][columns];
+int matrixtadd[rows][columns];
+// 17ETCS002124 K Srikanth
+using namespace std;
+void *addition(int rownumber)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        matrixtadd[rownumber][i] = matrixt1[rownumber][i] + matrixt2[rownumber][i];
+    }
+}
+
+int main()
+{
+    cout << "**************** K Srikanth 17ETCS002124 ****************** " <<endl;
+    cout <<endl;
+    thread threads[threads_count];
+    int  i, j;
+    srand(1);
+    for (int a = 0; a < rows; a++)
+    {
+        for (int b = 0; b < columns; b++)
+        {
+            matrixt1[a][b] = rand() % 5;
+            matrixt2[a][b] = rand() % 5;
+        }
+    }
+
+    for (int i = 0; i < threads_count; i++)
+    {
+        printf("Creating a thread in main %d \n", i + 1);
+        for (int i = 0; i < threads_count; i++)
+        {
+            threads[i] = thread{&addition, i};
+        }
+        for (int i = 0; i < threads_count; i++)
+        {
+            threads[i].join();
+        }
+    }
+
+    cout << "This is Matrix 1" << endl;
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf("%3d", matrixt1[i][j]);
+        }
+        cout
+            << endl;
+    }
+    cout
+        << "This is Matrix 2" << endl;
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf("%3d", matrixt2[i][j]);
+        }
+        cout << endl;
+    }
+    cout
+        << "This is Addition of Matrix 1 and Matrix 2" << endl;
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            matrixtadd[i][j] = matrixt1[i][j] + matrixt2[i][j];
+            printf("%4d", matrixtadd[i][j]);
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
