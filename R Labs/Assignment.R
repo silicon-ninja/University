3ed54c2d5f0872f16074f46d6fa5c7375bda736c
Srikanth <Srikanthkandarp23@gmail.com>
2021-02-06 11:12:36 +0530

    Probability Assignment Code Updated

--- R Labs/Assignment.R
+++ R Labs/Assignment.R
@@ -1,7 +1,12 @@
-data <- data.frame (Price=c(120, 106 ,95 ,70 ,65, 58),Number_Ordered=c(90, 115 ,121, 138, 155, 182))
+# K Srikanth 17ETCS002124
+data <- data.frame (Number_Ordered=c(90, 115 ,121, 138, 155, 182),Price=c(120, 106 ,95 ,70 ,65, 58))
 data
-plot(data$Price,data$Number_Ordered)
-linmod = lm(data$Number_Ordered ~ data$Price)
-abline(data$Price ~ data$Number_Ordered)
+plot(data$Number_Ordered,data$Price)
+linmod = lm(data$Price ~ data$Number_Ordered)
+abline(data$Number_Ordered ~ data$Price)
 abline(linmod, col="blue")
 linmod
+
+
+
+
