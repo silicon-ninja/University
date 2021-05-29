0224c97ccff2c5d089b21cd04dc0d5f1db164ffc
Srikanth Kandarp <48326585+Srikanth-Kandarp@users.noreply.github.com>
2021-05-29 19:05:14 +0530

    Updated the code to make it different from others

--- Assignments/Assignments 1/Codes/DS Code/Assignment_1/src/Assignment_Client.java
+++ Assignments/Assignments 1/Codes/DS Code/Assignment_1/src/Assignment_Client.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class Assignment_Client {
 
 	// 17ETCS002124 K Srikanth
-
 	public static void main(String[] args) throws IOException {
 		InetAddress ip = InetAddress.getLocalHost();
 		int port = 8080;
@@ -16,19 +15,60 @@ public class Assignment_Client {
 		Socket s = new Socket(ip, port);
 		DataInputStream dis = new DataInputStream(s.getInputStream());
 		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-
-		// 17ETCS002124 K Srikanth
+		System.out.println("");
+		System.out.println("****** 17ETCS002124 K Srikanth **************");
+		System.out.println("****** Client Side **************");
 
 		while (true) {
-			System.out.print("Enter the caculation: ");
-
-			String inp = sc.nextLine();
-
-			if (inp.equals("bye"))
+			System.out.println("");
+			System.out.print("Enter the Inputs\n");
+			System.out.print("*****************************************\n");
+			System.out.print("Enter the First Number\n");
+			String input_1 = sc.nextLine();
+			System.out.print("Enter the Second Number\n");
+			String input_2 = sc.nextLine();
+			System.out.print("********* Calculator *********\n");
+			System.out.print("Press 1 For Addition\n");
+			System.out.print("Press 2 For Subtraction\n");
+			System.out.print("Press 3 For Multiplication\n");
+			System.out.print("Press 4 For Division\n");
+			System.out.print("Press 5 to exit\n");
+			String choice = sc.nextLine();
+			dos.writeUTF(input_1);
+			dos.writeUTF(input_2);
+			if (choice.equals("1")) {
+				dos.writeUTF(choice);
+				String ans = dis.readUTF();
+				System.out.println("The Addition you're looking for is " + ans);
+				continue;
+			}
+			if (choice.equals("2")) {
+				dos.writeUTF(choice);
+				String ans = dis.readUTF();
+				System.out.println("The Subtraction you're looking for is " + ans);
+				continue;
+			}
+			if (choice.equals("3")) {
+				dos.writeUTF(choice);
+				String ans = dis.readUTF();
+				System.out.println("The Multiplication you're looking for is " + ans);
+				continue;
+			}
+			if (choice.equals("4")) {
+				dos.writeUTF(choice);
+				String ans = dis.readUTF();
+				System.out.println("The Division you're looking for is " + ans);
+				continue;
+			}
+			if (choice.equals("5")) {
+				dos.writeUTF(choice);
+				String ans = dis.readUTF();
+				System.out.println("The Answer you're looking for is " + ans);
 				break;
-			dos.writeUTF(inp);
-			String ans = dis.readUTF();
-			System.out.println("Answer=" + ans);
+			}
+
 		}
+
 	}
+
 }
--- Assignments/Assignments 1/Codes/DS Code/Assignment_1/src/Assignment_Server.java
+++ Assignments/Assignments 1/Codes/DS Code/Assignment_1/src/Assignment_Server.java
@@ -6,36 +6,57 @@ import java.net.Socket;
 import java.util.StringTokenizer;
 
 public class Assignment_Server {
-
 	// 17ETCS002124 K Srikanth
 	public static void main(String args[]) throws IOException {
 
-		ServerSocket ss = new ServerSocket(8080);
-		Socket s = ss.accept();
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		ServerSocket sockerobj = new ServerSocket(8080);
+		Socket soccon = sockerobj.accept();
+		DataInputStream data_input_stream = new DataInputStream(soccon.getInputStream());
+		DataOutputStream data_output_stream = new DataOutputStream(soccon.getOutputStream());
+		System.out.println("");
+		System.out.println("****** 17ETCS002124 K Srikanth **************");
+		System.out.println("****** Server Side **************");
+
 		while (true) {
-			// 17ETCS002124 K Srikanth
-
-			String input = dis.readUTF();
-			System.out.println("Input received:-" + input);
+			String in1 = data_input_stream.readUTF();
+			String in2 = data_input_stream.readUTF();
+			String choice = data_input_stream.readUTF();
+			System.out.println("Received Data from Client");
 			int result = 0;
-			StringTokenizer st = new StringTokenizer(input);
-			int Input_1 = Integer.parseInt(st.nextToken());
-			String operation = st.nextToken();
-			int Input_2 = Integer.parseInt(st.nextToken());
-			if (operation.equals("+")) {
-				result = Input_1 + Input_2;
-			} else if (operation.equals("-")) {
-				result = Input_1 - Input_2;
-			} else if (operation.equals("*")) {
-				result = Input_1 * Input_2;
-			} else if (operation.equals("/")) {
-				result = Input_1 / Input_2;
+			int Input_1 = Integer.parseInt(in1);
+			System.out.println("Srikanth's Input 1 is : " + Input_1);
+			int Input_2 = Integer.parseInt(in2);
+			System.out.println("Srikanth's Input 2 is : " + Input_2);
+			int switch_choice = Integer.parseInt(choice);
+			System.out.println("Srikanth's Choice is : " + switch_choice);
+			System.out.println("");
+			System.out.println("");
+			switch (switch_choice) {
+				case 1:
+					result = Input_1 + Input_2;
+					data_output_stream.writeUTF(String.valueOf(result));
+					break;
+				case 2:
+					result = Input_1 - Input_2;
+					data_output_stream.writeUTF(String.valueOf(result));
+					break;
+				case 3:
+					result = Input_1 * Input_2;
+					data_output_stream.writeUTF(String.valueOf(result));
+					break;
+				case 4:
+					result = Input_1 / Input_2;
+					data_output_stream.writeUTF(String.valueOf(result));
+					break;
+				case 5:
+					String bye_String = "Good Bye From Server Hahah XD";
+					data_output_stream.writeUTF(bye_String);
+					break;
+				default:
+					System.out.println("Invalid Input Please Try Again");
+					// code block
 			}
-			System.out.println("Sending the Caculation to Client");
-
-			dos.writeUTF(String.valueOf(result));
 		}
+
 	}
 }
